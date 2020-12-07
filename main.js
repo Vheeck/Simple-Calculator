514172cb2bc66ee3197fcf87a3d423f259915a91
Vheeck <momohvictor62@gmail.com>
2020-12-07 12:25:04 +0100

    Added main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/* 
+    JavaScript
+
+    1.  Variables
+            a. var name = value; 
+            b. let name = value; ECMA Script
+            c. const name = value;
+    2.  Operations
+    3.  Methods/Functions
+    4.  Classes (OOP - Object Oriented Programming)
+    5.  
+
+*/
